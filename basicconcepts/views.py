1838685dc420c768e165aebf04c97a144cf8a59a
Rafael Mello <rafaelmello0715@outlook.com>
2020-04-12 02:56:46 -0300

    Using templates, static files and media files

--- basicconcepts/views.py
+++ basicconcepts/views.py
@@ -1,16 +1,33 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 class User:
     def index(request):
-        return HttpResponse(['Rafael', 'Rodrigo'])
+        return render(request, 'index.html')
 
     def show(request, id):
         user_list = {
             1: {
                 'id': 1,
                 'name': 'Rafael de Mello e Sousa'
+            },
+
+            2: {
+                'id': 2,
+                'name': 'Fernando Geraldo Oliveira'
             }
         }
 
-        return HttpResponse(str(user_list[id]['id']) + ' - ' + user_list[id]['name'])
+        user = {}
+
+        try:
+            user = {
+                'id': user_list[id]['id'],
+                'name': user_list[id]['name']
+            }
+        except:
+            user = {
+                'error': 'User not found'
+            }
+
+        return render(request, 'user.html', user)
--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ class User(models.Model):
     email = models.EmailField(max_length=100)
     age = models.IntegerField()
     bio = models.TextField()
+    avatar = models.ImageField(upload_to='users', null=True, blank=True)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
